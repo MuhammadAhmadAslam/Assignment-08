d90c1b96f75a18ddd697320e4b3392ffa3b7380d
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-04-22 23:35:14 +0500

    Assignment 08

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ console.log(reversedStr);
 
 
 
-let number = '10'
+let number = 12;
 let isPrime = true
 if(number <= 1){
     console.log('not a prime number');
@@ -32,5 +32,78 @@ if(number <= 1){
         }
 }
 }
-console.log(isPrime);
+if(isPrime == false){
+    console.log(`${number} is a prime number`);
+}
 
+let numberList = [0, 2 ,1 , 12 , 8, 5 , 9 ,3 ,2 ,13 ,15];
+let maxNumber = numberList[0];
+for(let i = 0 ; i < numberList.length; i++){
+  if (numberList[i] > maxNumber) {
+    maxNumber = numberList[i]
+  }
+}
+console.log(maxNumber);
+
+let palindrome = 'oppo'
+let reversePalindrome = ''
+for (let i = palindrome.length-1; i >= 0; i--) {
+        reversePalindrome += palindrome
+        break
+}
+console.log(reversePalindrome);
+if (palindrome == reversePalindrome) {
+    console.log(`This letter ${palindrome} is palindrome`);
+}
+
+let n1 = 0 ; n2 = 1; 
+let nextTerm;
+for (let i = 0; i <= 20; i++) {
+  console.log(n1);//0 // 1
+  nextTerm = n1 + n2;//0+1 = 1 // 1 + 0 = 1
+  n1 = n2 // 0 = 1 
+  n2 = nextTerm // 0 = 0
+}
+console.log(nextTerm);
+
+
+let vowelsCounting = 'Hello how are you ? i am fine'.toLowerCase();
+let vowels = ['a' ,'e','i','o','u'];
+let count = 0
+for (let i = 0; i <= vowelsCounting.length; i++) {
+  for (let j = 0; j <= vowels.length; j++) {
+    if (vowelsCounting[i] == vowels[i]) {
+     count++
+    }
+    
+  }
+  
+}
+console.log(count);
+
+
+
+
+let factorialNumber = 5;
+let newnum = 1
+for (let i = 1; i  <= factorialNumber; i++) {
+  newnum *= i
+  
+}
+
+console.log(newnum);
+
+
+
+let listOfNumbers = '12345'
+let convert = listOfNumbers.split('')
+let add = 0
+console.log(convert);
+for (let i = 0; i < convert.length; i++) {
+   add += parseInt(convert[i])
+  
+}
+console.log(add);
+// console.log(convert);
+let reverseInteger = convert.reverse('')
+console.log(reverseInteger);
